8a53bfc41ef8d252d3414a84b350d0d1814e747a
deemoe404 <me@dee.moe>
2024-02-24 01:48:44 +0800

    Update injector.js

--- scripts/injector.js
+++ scripts/injector.js
@@ -1,1 +1,2 @@
 hexo.extend.injector.register('body_end', '<script src="/js/outdate.js"></script>', 'post')
+hexo.extend.injector.register('body_begin', '<a rel="me" href="https://c7.io/@meow">Mastodon</a>', 'home')
